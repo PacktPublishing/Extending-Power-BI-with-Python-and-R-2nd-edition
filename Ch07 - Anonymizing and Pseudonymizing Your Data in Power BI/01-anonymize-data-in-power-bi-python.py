ee465d3eff9b0775a53b6439964ff2f545049074
Luca Zavarella <lucazavarella@outlook.com>
2023-02-27 10:47:18 +0100

    baseline for chapter 7

---
+++ Ch07 - Anonymizing and Pseudonymizing Your Data in Power BI/01-anonymize-data-in-power-bi-python.py
@@ -0,0 +1,90 @@
+
+from presidio_analyzer import AnalyzerEngine
+from presidio_anonymizer import AnonymizerEngine
+from presidio_anonymizer.entities.engine import OperatorConfig
+
+import numpy as np
+import secrets
+import string
+
+
+def generateToken(len):
+    alphabet = string.ascii_letters + string.digits
+    tkn = ''.join(secrets.choice(alphabet) for i in range(len))
+    return tkn
+
+
+# Function used to anonymize a text containing emails
+def anonymizeEmail(text_to_anonymize): 
+    if text_to_anonymize != "nan" and len(text_to_anonymize) != 0 and not text_to_anonymize.isspace():
+        
+        analyzer_results = analyzer.analyze(text=text_to_anonymize, entities=["EMAIL_ADDRESS"], language='en')
+
+        anonymized_results = anonymizer.anonymize(
+            text=text_to_anonymize,
+            analyzer_results=analyzer_results,    
+            operators={"EMAIL_ADDRESS": OperatorConfig("replace", {"new_value": generateToken(20)})}
+        )
+        
+        output_text = anonymized_results.text
+    elif text_to_anonymize == "nan":
+        output_text = np.NaN
+    else:
+        output_text = text_to_anonymize
+
+    return output_text
+
+# Function used to anonymize a text containing names
+def anonymizeName(text_to_anonymize): 
+    if text_to_anonymize != "nan" and len(text_to_anonymize) != 0 and not text_to_anonymize.isspace():
+        
+        analyzer_results = analyzer.analyze(text=text_to_anonymize, entities=["PERSON"], language='en')
+
+        anonymized_results = anonymizer.anonymize(
+            text=text_to_anonymize,
+            analyzer_results=analyzer_results,    
+            operators={"PERSON": OperatorConfig("replace", {"new_value": generateToken(20)})}
+        )
+        
+        output_text = anonymized_results.text
+    elif text_to_anonymize == "nan":
+        output_text = np.NaN
+    else:
+        output_text = text_to_anonymize
+        
+    return output_text
+
+
+# For testing purpose you can load the Excel content directly here.
+# Just uncomment the following 3 lines.
+# # Load the Excel content in a dataframe
+# import pandas as pd
+# dataset = pd.read_excel(r'D:\<your-path>\Ch07 - Anonymizing and Pseudonymizing Your Data in Power BI\CustomersCreditCardAttempts.xlsx', engine='openpyxl')
+
+
+# Initialize Presidio's analyzer and anonymizer
+# https://microsoft.github.io/presidio/supported_entities/
+analyzer = AnalyzerEngine()
+anonymizer = AnonymizerEngine()
+
+# Create a copy of the source dataset
+df = dataset.copy()
+
+# Apply the function anonymizeName for each value of the Name column
+df.Name = df.Name.apply(lambda x: anonymizeName(x))
+
+# Apply the function anonymizeEmail for each value of the Email column
+df.Email = df.Email.apply(lambda x: anonymizeEmail(x))
+
+# Column Notes is 'object' data type as it contains lot of NaN and
+# Pandas doesn't recognize it as string. So it has to be cast to string
+# in order to be anonymized. Then replace it with its anonymization
+df.Notes = df.Notes.astype('str').apply(lambda x: anonymizeName(x) )
+df.Notes = df.Notes.astype('str').apply(lambda x: anonymizeEmail(x) )
+
+# # Prevent Pandas to truncate strings in cells
+# pd.set_option('display.max_colwidth', None)
+
+# # Show both the dataframes
+# dataset
+# df
---
+++ Ch07 - Anonymizing and Pseudonymizing Your Data in Power BI/03-pseudonymize-data-in-power-bi-python.py
@@ -0,0 +1,140 @@
+import os
+import pickle
+import pandas as pd
+import numpy as np
+
+from presidio_analyzer import AnalyzerEngine
+from presidio_anonymizer import AnonymizerEngine
+
+from faker import Faker
+from faker.providers import internet
+
+
+# Function used to pseudonymize a text containing emails
+def anonymizeEmail(text_to_anonymize, country):
+    if text_to_anonymize != "nan" and len(text_to_anonymize) != 0 and not text_to_anonymize.isspace():
+        # Initialize Faker
+        fake = Faker(faker_locales_dict[country])
+        fake.add_provider(internet)
+
+        analyzer_results = analyzer.analyze(text=text_to_anonymize, entities=["EMAIL_ADDRESS"], language='en')
+        
+        matched_emails = {}
+        for match in analyzer_results:
+            email = text_to_anonymize[match.start:match.end]
+            
+            if email not in emails_dict:
+                fake_email = fake.safe_email()
+
+                while (fake_email in emails_dict.values()) or (fake_email in emails_dict):
+                    fake_email = fake.safe_email()
+                    
+                emails_dict[email] = fake_email
+                matched_emails[email] = fake_email
+            else:
+                fake_email = emails_dict[email]
+                matched_emails[email] = fake_email
+
+        anonymized_result = text_to_anonymize
+        for email in matched_emails:
+            anonymized_result = anonymized_result.replace(email, matched_emails[email])
+
+    elif text_to_anonymize == "nan":
+        anonymized_result = np.NaN
+    else:
+        anonymized_result = text_to_anonymize
+        
+    return anonymized_result
+
+# Function used to pseudonymize a text containing names
+def anonymizeName(text_to_anonymize, country):
+    if text_to_anonymize != "nan" and len(text_to_anonymize) != 0 and not text_to_anonymize.isspace():
+        # Initialize Faker
+        fake = Faker(faker_locales_dict[country])
+        
+        analyzer_results = analyzer.analyze(text=text_to_anonymize, entities=["PERSON"], language='en')
+
+        matched_names = {}
+        for match in analyzer_results:
+            name = text_to_anonymize[match.start:match.end]
+            
+            if name not in names_dict:
+                fake_name = fake.name()
+                
+                while (fake_name in names_dict.values()) or (fake_name in names_dict):
+                    fake_name = fake.name()
+
+                names_dict[name] = fake_name
+                matched_names[name] = fake_name
+            else:
+                fake_name = names_dict[name]
+                matched_names[name] = fake_name
+
+        anonymized_result = text_to_anonymize
+        for name in matched_names:
+            anonymized_result = anonymized_result.replace(name, matched_names[name])
+
+    elif text_to_anonymize == "nan":
+        anonymized_result = np.NaN
+    else:
+        anonymized_result = text_to_anonymize
+        
+    return anonymized_result
+
+
+# For testing purpose you can load the Excel content directly here.
+# Just uncomment the following 2 lines.
+# # Load the Excel content in a dataframe
+# dataset = pd.read_excel(r'D:\<your-path>\Chapter06\CustomersCreditCardAttempts.xlsx', engine='openpyxl')
+
+# Load mapping dictionaries from PKL files if they exist, otherwise create empty dictionaries
+pkls_path = r'C:\Users\lucazav\OneDrive\MVP\PacktBook\Code\Extending-Power-BI-with-Python-and-R-2nd-edition\Ch07 - Anonymizing and Pseudonymizing Your Data in Power BI\pkls'
+emails_dict_pkl_path = os.path.join(pkls_path, 'emails_dict.pkl')
+names_dict_pkl_path = os.path.join(pkls_path, 'names_dict.pkl')
+
+if os.path.isfile(emails_dict_pkl_path):
+    emails_dict = pickle.load( open(emails_dict_pkl_path, "rb") )
+else:
+    emails_dict = {}
+
+if os.path.isfile(names_dict_pkl_path):
+    names_dict = pickle.load( open(names_dict_pkl_path, "rb") )
+else:
+    names_dict = {}
+
+# Define locale and language dictionaries
+faker_locales_dict = {'UNITED STATES': 'en_US', 'ITALY': 'it_IT', 'GERMANY': 'de_DE'}
+
+
+
+# Initialize Presidio's analyzer and anonymizer
+# https://microsoft.github.io/presidio/supported_entities/
+analyzer = AnalyzerEngine()
+anonymizer = AnonymizerEngine()
+
+# Create a copy of the source dataset
+df = dataset.copy()
+
+# Apply the function anonymizeName for each value of the Name column
+df.Name = pd.Series( [anonymizeName(text, country) for (text, country) in zip(df['Name'], df['Country'])] )
+
+# Apply the function anonymizeEmail for each value of the Email column
+df.Email = pd.Series( [anonymizeEmail(text, country) for (text, country) in zip(df['Email'], df['Country'])] )
+
+# Column Notes is 'object' data type as it contains lot of NaN and
+# Pandas doesn't recognize it as string. So it has to be cast to string
+# in order to be anonymized. Then replace it with its anonymization
+df.Notes = pd.Series( [anonymizeName(text, country) for (text, country) in zip(df['Notes'].astype('str'), df['Country'])] )
+df.Notes = pd.Series( [anonymizeEmail(text, country) for (text, country) in zip(df['Notes'].astype('str'), df['Country'])] )
+
+
+# # Prevent Pandas to truncate strings in cells
+# pd.set_option('display.max_colwidth', None)
+
+# # Show both the dataframes
+# dataset
+# df
+
+# Write emails and names dictionaries to PKL files
+pickle.dump( emails_dict, open(emails_dict_pkl_path, "wb") )
+pickle.dump( names_dict, open(names_dict_pkl_path, "wb") )
